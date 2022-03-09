00e92701ebfa2804c8bc7e57abbf73d3455896a4
Amanda Sandoval <99850511+AmandaSandoval@users.noreply.github.com>
2022-03-08 22:21:47 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Entrada
+var a = prompt ("digite o primeiro número");
+var b = prompt ("Digite o segundo número");
+
+// Processamento
+var r = parseInt (a) + parseInt (b);
+
+//Saída
+alert("Resultado: "+ r);
